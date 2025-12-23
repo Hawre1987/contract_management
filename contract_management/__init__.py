3e59cda017e5ff9f5f49f1ec4af372722f875576
Hawre Salih <106107046+Hawre1987@users.noreply.github.com>
2025-12-23 11:33:29 +0300

    Applying the necessary changes to allow the app installation on frappe cloud.

--- contract_management/__init__.py
+++ contract_management/__init__.py
@@ -1,1 +1,15 @@
+from __future__ import unicode_literals
 
+# REMOVE the top-level frappe.utils import from here
+
+__version__ = '0.0.1'
+
+def sample():
+    # Move the imports INSIDE the function
+    import frappe
+    from frappe.utils import cint
+    
+    # Now it only tries to find frappe when sample() is actually called,
+    # not when the app is being installed.
+    is_inclusive = cint(frappe.get_single("Accounts Settings").show_inclusive_tax_in_print)
+    print(is_inclusive)
